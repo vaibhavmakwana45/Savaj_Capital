2b030263acdc8d976cad192cd2eeb34e0677897c
keybrains <kbtplseo@gmail.com>
2024-05-16 18:54:34 +0530

    addButton

--- src/files/ViewFile.js
+++ src/files/ViewFile.js
@@ -1078,6 +1078,7 @@ function ViewFile() {
                                       )}
                                     </FormControl>
                                   )
+                                  
                                 )}
                                 <Button
                                   colorScheme="blue"
